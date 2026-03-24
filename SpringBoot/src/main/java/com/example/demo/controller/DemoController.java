c106bc469dbdca850821fcfd33f09f0c1141e59e
domingoLopezOller <domingolopez.instituto@gmail.com>
2026-03-24 11:40:20 +0100

    springboot

--- SpringBoot/src/main/java/com/example/demo/controller/DemoController.java
+++ SpringBoot/src/main/java/com/example/demo/controller/DemoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -20,6 +21,11 @@ public class DemoController {
     @GetMapping("/contacto")
     public String contacto(Model model) {
         return "contacto";
+    }
+    @GetMapping("/ciclos/{id}")
+    public String ciclo(@PathVariable String id, Model model) {
+        model.addAttribute("ciclo", id);
+        return "ciclo";
     }
     @GetMapping("/asir")
     public String asir(Model model) {
